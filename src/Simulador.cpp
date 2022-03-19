3e5bd34b380ade45cef8e2d7f0359c335a1810a1
titosilva <jt.mat@hotmail.com>
2022-03-19 18:14:49 -0300

    feat: split messages into bits

--- src/Simulador.cpp
+++ src/Simulador.cpp
@@ -1,9 +1,11 @@
 #include "Simulador.hpp"
-#include "CamadaFisica.hpp"
 
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
+
+#include "CamadaFisica.hpp"
 
 using namespace std;
 
@@ -17,7 +19,19 @@ void AplicacaoTransmissora(void) {
 }
 
 void CamadaDeAplicacaoTransmissora(string mensagem) {
+    // Transformar em bits
     vector<int> quadro;
+    for (int i = 0; i < mensagem.length(); i++) {
+        auto c = mensagem[i];
+        quadro.push_back((c >> 0) & 0x1);
+        quadro.push_back((c >> 1) & 0x1);
+        quadro.push_back((c >> 2) & 0x1);
+        quadro.push_back((c >> 3) & 0x1);
+        quadro.push_back((c >> 4) & 0x1);
+        quadro.push_back((c >> 5) & 0x1);
+        quadro.push_back((c >> 6) & 0x1);
+        quadro.push_back((c >> 7) & 0x1);
+    }
 
     // Chama a próxima camada
     CamadaFisicaTransmissora(quadro);
@@ -83,12 +97,31 @@ void CamadaFisicaReceptora(vector<int> quadro) {
 }
 
 void CamadaDeAplicacaoReceptora(vector<int> quadro) {
+    vector<int>::iterator iter = quadro.begin();
     string mensagem = "";
+
+    auto currentBit = 0;
+    char currentChar = 0;
+    while (iter != quadro.end()) {
+        auto bit = *iter;
+        currentChar |= (bit << currentBit);
+
+        if (currentBit == 7) {
+            mensagem.push_back(currentChar);
+            currentBit = 0;
+            currentChar = 0;
+        } else {
+            currentBit++;
+        }
+
+        iter++;
+    }
+
 
     // Chama a próxima camada
     AplicacaoReceptora(mensagem);
 }
 
 void AplicacaoReceptora(string mensagem) {
-    cout << "A mensagem recebida foi:" << mensagem << endl;
+    cout << "A mensagem recebida foi:" << endl << mensagem << endl;
 }
