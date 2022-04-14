213e43ee393481a61907aa405d7d4895c2a53063
titosilva <jt.mat@hotmail.com>
2022-04-13 23:54:36 -0300

    fix: improved CamadaEnlace by adding prints

--- src/CamadaEnlace/CamadaEnlace.cpp
+++ src/CamadaEnlace/CamadaEnlace.cpp
@@ -3,17 +3,26 @@
 #include <vector>
 
 #include "../Camadas.hpp"
+#include "../Utilitario.hpp"
 
 using namespace std;
 
 void CamadaEnlace::ReceberDados(vector<int> quadro) {
     auto quadroRecuperado = this->enquadrador->Desenquadrar(quadro);
 
+    Imprime("Camada de enlace - quadro recebido após ser desenquadrado:");
+    Imprime(quadroRecuperado);
+    NovaLinha();
+
     this->camadaSuperior->ReceberDados(quadroRecuperado);
 }
 
 void CamadaEnlace::TransmitirDados(vector<int> quadro) {
     auto quadroEnquadrado = this->enquadrador->Enquadrar(quadro);
+
+    Imprime("Camada de enlace - quadro a ser transmitido após ser enquadrado:");
+    Imprime(quadroEnquadrado);
+    NovaLinha();
 
     this->camadaInferior->TransmitirDados(quadroEnquadrado);
 }
