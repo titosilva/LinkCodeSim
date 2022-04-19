58f4c925ee8fd8e100f1f138e134e8b67b59f53f
titosilva <jt.mat@hotmail.com>
2022-04-18 21:55:08 -0300

    feat: added randomError to Meio de Comunicacao

--- src/Simulador.cpp
+++ src/Simulador.cpp
@@ -5,17 +5,26 @@
 #include <limits>
 #include <string>
 #include <vector>
+#include <random>
 
 #include "Camadas.hpp"
 
 using namespace std;
 
-void MeioDeComunicacao(vector<int> quadro, Maquina* maquinaReceptora) {
-    vector<int> fluxoBrutoDeBitsPontoA = quadro, fluxoBrutoDeBitsPontoB;
+void MeioDeComunicacao(vector<int> fluxoBrutoDeBits, Maquina* maquinaReceptora) {
+    int erro, porcentagemDeErros;
+    vector<int> fluxoBrutoDeBitsPontoA = fluxoBrutoDeBits, fluxoBrutoDeBitsPontoB;
+
+    porcentagemDeErros = 0;
 
     int i = 0;
     while (fluxoBrutoDeBitsPontoA.size() > fluxoBrutoDeBitsPontoB.size()) {
-        fluxoBrutoDeBitsPontoB.push_back(fluxoBrutoDeBitsPontoA[i]);
+        if ((rand()%100) < porcentagemDeErros) {
+            // Se estiver na "faixa" de erros, introduz erro
+            fluxoBrutoDeBitsPontoB.push_back(fluxoBrutoDeBitsPontoA[i] == 0? fluxoBrutoDeBitsPontoA[i] + 1 : fluxoBrutoDeBitsPontoA[i] - 1);
+        } else {
+            fluxoBrutoDeBitsPontoB.push_back(fluxoBrutoDeBitsPontoA[i]);
+        }
         i++;
     }
 
